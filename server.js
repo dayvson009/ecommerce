53288c5197edf87699629f41d143ed1394101d66
unknown <dayvson.vinicius@hotmail.com>
2024-12-17 21:00:16 -0300

    upload2

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
-console.log("Hello Vercel - Ecommerce")
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000; // Porta definida pela Vercel ou 3000
+
+// Middleware para interpretar JSON
+app.use(express.json());
+
+// Rota inicial
+app.get('/', (req, res) => {
+    res.send('Hello World! Bem-vindo ao meu e-commerce.');
+});
+
+// Rotas do E-commerce
+app.get('/produtos', (req, res) => {
+  res.json({ mensagem: 'Listagem de produtos' });
+});
+
+app.post('/checkout', (req, res) => {
+  const pedido = req.body; // Simulação de recebimento de pedido
+  res.json({ mensagem: 'Pedido recebido!', pedido });
+});
+
+app.get('/status-pedido/:id', (req, res) => {
+  const pedidoId = req.params.id;
+  res.json({ mensagem: `Status do pedido ${pedidoId}`, status: 'Processando' });
+});
+
+// Inicia o servidor
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+module.exports = app; // Exporta para a Vercel
