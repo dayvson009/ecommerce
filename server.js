3be38a5ae6966e92217b3a382ab59ca009304eea
unknown <dayvson.vinicius@hotmail.com>
2024-12-21 20:22:14 -0300

    Adicionando o Path no caminho da views para reconhecer o ejs

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const app = express();
 const routes = require('./routes');
 const cors = require('cors');
 const PORT = process.env.PORT || 3000;
+const path = require('path');
+
+
 
 app.use(cors());
 
 // Configuração do motor de template EJS
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 // Middleware para interpretar JSON
 app.use(express.json());
 
